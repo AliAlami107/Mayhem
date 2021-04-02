25ddc545847e4b66da29215f58b27d5f00008ea8
kasparah <72358740+kasparah@users.noreply.github.com>
2021-04-02 22:03:55 +0200

    Add files via upload

---
+++ mayhem.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+#Importing the needed modules/libraries
+import pygame
+
+#initizling pygame
+pygame.init()
+Clock = pygame.time.Clock()
+#Defining window size and etc.
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption('Mayhem')
+background = pygame.image.load("bg.png").convert()
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+#Colors
+BLUE = (50,50,255)
+"""
+class ():
+    def __init__(self):
+        self.x = x
+
+"""
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.blit(background, [0,0])
+
+    pygame.display.update()
+    Clock.tick(60)
