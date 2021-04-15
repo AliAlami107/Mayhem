085de61a223efeecf62dd6ab0b7b4d32f8e7f5f8
kasparah <72358740+kasparah@users.noreply.github.com>
2021-04-15 19:10:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Authors: Ali Alami, Kasper Hansen
+This module contains the idiom 'if __name__ == '__main__':'. 
+Here the program is run directly.
+"""
+
+import primary
+
+
+if __name__ == '__main__':
+    primary.mayhem
